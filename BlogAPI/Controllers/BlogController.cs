b65215b32748050525e67959fe62ff759ac67518
Dilshan Prasad <dilshanprasadbrown@gmail.com>
2020-08-26 12:48:49 +0530

    Add project files.

---
+++ BlogAPI/Controllers/BlogController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using BlogAPI.Entity;
+
+namespace BlogAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlogController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult getBlog() {
+
+            var blog = new Blog
+            {
+                id = 1,
+                title = "First Post",
+                content = "This is my first blog post"
+            };
+
+            return Ok(blog);
+        }
+    }
+}
---
+++ BlogAPI/Entity/Blog.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogAPI.Entity
+{
+    public class Blog
+    {
+        public int id { get; set; }
+        public String title{ get; set; }
+        public String content{ get; set; }
+        public String imgUrl { get; set; }
+    }
+}
